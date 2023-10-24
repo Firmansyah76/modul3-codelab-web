0c1dcbb94717cf9c463c1e1a349b357fbe85db4a
Firmansyah76 <muhammadfirmansyah2718@gmail.com>
2023-10-24 14:30:26 +0700

    first commit

---
+++ Latmodul3/components/Footer.jsx
@@ -0,0 +1,13 @@
+// src/components/Footer.js
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <p>&copy; RM surya indah</p>
+      <p>firman</p>
+    </footer>
+  );
+}
+
+export default Footer;
---
+++ Latmodul3/components/Hero.jsx
@@ -0,0 +1,13 @@
+// src/components/Hero.js
+import React from 'react';
+
+function Hero() {
+  return (
+    <div>
+      <h1>selamat datang di RM Surya Indah</h1>
+      <p>Firman</p>
+    </div>
+  );
+}
+
+export default Hero;
